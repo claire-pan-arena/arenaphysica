69380f28805c6f35b31d3b94b510264d4052d78a
claire-pan-arena <claire-pan-arena@users.noreply.github.com>
2026-03-13 23:00:40 -0400

    Initial commit: black screen with Arena Physica

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl font-light tracking-widest text-white uppercase">
+        Arena Physica
+      </h1>
+    </div>
+  );
+}
